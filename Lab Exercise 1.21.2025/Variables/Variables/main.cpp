8557eff91b75adbe7338ecd4c289700744418af7
nmessa <nmessa@sau16.org>
2025-01-20 13:36:31 -0500

    Add files via upload

---
+++ Lab Exercise 1.21.2025/Variables/Variables/main.cpp
@@ -0,0 +1,20 @@
+//Lab Exercise 1.21.2025 Problem 4
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//Add code here
+
+
+
+	return 0;
+}
+
+//Output
+//42
+//210
+//This is a string

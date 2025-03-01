e501f4400d6d95510d9f03f29dbfa0ce5e45e4b9
nmessa <nmessa@sau16.org>
2025-03-01 10:19:25 -0500

    Add files via upload

---
+++ Lab Exercise 3.3.2025/Problem 4/Problem 4/main.cpp
@@ -0,0 +1,23 @@
+//Lab Exercise 3.3.2025 Problem 4
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Function prototype
+int count_consonants(string);
+
+int main()
+{
+	string test = "This is a test of the count_consonants function";
+	cout << count_consonants(test) << endl; //25
+
+	return 0;
+}
+
+int count_consonants(string str)
+{
+    //Add code here
+
+}

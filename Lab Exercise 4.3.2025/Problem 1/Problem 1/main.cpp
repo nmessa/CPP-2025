e1a6e427a712fa2c6730f717ea2bfe9cb56c7a3f
nmessa <nmessa@sau16.org>
2025-03-28 13:29:29 -0400

    Add files via upload

---
+++ Lab Exercise 4.3.2025/Problem 1/Problem 1/main.cpp
@@ -0,0 +1,21 @@
+//Lab Exercise 4.3.2025 Problem 1
+//Author: 
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	
+
+
+	return 0;
+}
+
+
+
+//Output
+//17
+//11
+//10
---
+++ Lab Exercise 4.3.2025/Problem 2/Problem 2/main.cpp
@@ -0,0 +1,20 @@
+//Lab Exercise 4.3.2025 Problem 2
+//Author: 
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+
+
+	return 0;
+}
+
+
+
+//Output
+//36
+//22
+//50
